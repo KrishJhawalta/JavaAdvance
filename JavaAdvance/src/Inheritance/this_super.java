def5b800af31a0580b23c61adef8e2ff127954bc
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-15 17:21:37 +0530

    this & super keyword basics

---
+++ JavaAdvance/src/Inheritance/this_super.java
@@ -0,0 +1,33 @@
+package Inheritance;
+class employee{
+    int id;
+
+    employee(int id){
+        this.id=id;       // {this.} keyword reference the variable in constructor
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+class user extends employee{
+    int id;
+    user(int id){
+        super(id);      // {super} keyword uses parent keyword
+    }
+}
+public class this_super {
+    public static void main(String[] args) {
+        user u1 = new user(5);
+        System.out.println("Id is: "+u1.getId());
+
+    }
+}
+/* 1️⃣ Parent constructor call
+super(id);
+2️⃣ Parent variable use
+super.id
+3️⃣ Parent method call
+super.show();
+*/
+
