c5f93d7e61892faf08de812f74414318587db34b
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-20 21:22:20 +0530

    New override file

--- JavaAdvance/src/Inheritance/MethodOveriding.java
+++ JavaAdvance/src/Inheritance/MethodOveriding.java
@@ -12,6 +12,10 @@ class A{
 
 class B extends A{
 
+    public void meth2(){           // Using method overriding: meth2 of class A is changed in meth2 of class B
+        System.out.println("I am method 2 of class B");
+    }
+
     public void meth3(){
         System.out.println("I am method 3");
     }
@@ -20,6 +24,11 @@ public class MethodOveriding{
     public static void main(String[] args) {
          A a = new A();
          a.meth();
+         a.meth2();
+
+         B b = new B();
+         b.meth2();
+         b.meth3();
 
 
     }
