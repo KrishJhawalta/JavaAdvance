890aead4bdc9bc971ee1c628e7ad8d5cf2876fdc
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-22 21:47:40 +0530

    Basic of abstraction

---
+++ JavaAdvance/src/Abstraction/HowToAbs.java
@@ -0,0 +1,20 @@
+package Abstraction;
+
+abstract class base{
+    public base(){
+        System.out.println("I am constructor of class base");
+    }
+
+    public void sayHello(){
+        System.out.println("hello man");
+    }
+    abstract void greet();   // abstract method
+
+
+}
+public class HowToAbs {
+    static void main(String[] args) {
+
+    }
+
+}
