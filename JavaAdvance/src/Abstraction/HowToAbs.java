dedc9e8395f63d4d27768171d608ed576f6818a9
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-22 21:58:43 +0530

    Abstraction new

--- JavaAdvance/src/Abstraction/HowToAbs.java
+++ JavaAdvance/src/Abstraction/HowToAbs.java
@@ -1,19 +1,31 @@
 package Abstraction;
 
-abstract class base{
-    public base(){
+abstract class base {             // abstract class due to abstract method necessary
+    public base() {
         System.out.println("I am constructor of class base");
     }
 
-    public void sayHello(){
+    public void sayHello() {
         System.out.println("hello man");
     }
+
     abstract void greet();   // abstract method
+}
+class funk extends base{
+    @Override
+    public void greet(){
+        System.out.println("I am funk");
+    }
+}
 
-
+clas classy extends funk(){
+    public classy(){
+        System.out.println("I am classy");
+    }
 }
 public class HowToAbs {
     static void main(String[] args) {
+
 
     }
 
