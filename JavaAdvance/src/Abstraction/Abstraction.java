418c23dfce20a2df2bea712c6930b771fba83154
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-26 22:07:33 +0530

    abstraction example

---
+++ JavaAdvance/src/Abstraction/Abstraction.java
@@ -0,0 +1,39 @@
+class mobiles{
+
+
+
+    mobiles(){
+        System.out.println("Constructor of mobiles");
+    }
+
+    abstract void call();   // abstract method
+    abstract void message();
+
+}
+
+class Samsung extends mobiles{
+
+    @Override
+    void call(){
+        System.out.println("Calling samsung");
+    }
+
+    @Override
+    void message(){
+        System.out.println("Messaging from samsung");
+    }
+}
+
+
+
+
+
+
+
+public class Abstraction{
+    public static void main(String[] args) {
+        
+        
+    }
+
+}
