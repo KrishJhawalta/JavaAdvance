30d7deeffae20b4f6aae2b44ce38de95172c051e
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-29 21:50:19 +0530

    practice question

---
+++ JavaAdvance/src/PracticeQuestions/Q1.java
@@ -0,0 +1,23 @@
+abstract class Pen{
+    public Pen(){
+        System.out.println("I am Pen");
+    }
+
+     abstract void write();
+       
+    
+
+     abstract void refill();
+        
+    
+}
+
+
+public class Q1{
+    public static void main(String[]args){
+
+        // Question 1: Create a abstract class Pen with methods write(), refill() as abstract
+        
+        
+    }
+}
