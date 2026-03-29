2c8a9ecdc3c6d09de981eb1a0268af29e8a0ec02
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-29 21:54:34 +0530

    new practice

---
+++ JavaAdvance/src/PracticeQuestions/Q2.java
@@ -0,0 +1,15 @@
+package PracticeQuestions;
+
+class pen(){
+    
+}
+
+public class Q2 {
+    public static void main(String[]args){
+
+        // Question 2: Create a pen class from Q1 to create concrete class FountainPen with additional method changeNib();
+
+        
+    }
+    
+}
