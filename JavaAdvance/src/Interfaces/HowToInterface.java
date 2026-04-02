24a5295344e65d92644d4666ec67e1b3d30d9c09
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-02 21:59:10 +0530

    repeating construcotr

--- JavaAdvance/src/Interfaces/HowToInterface.java
+++ JavaAdvance/src/Interfaces/HowToInterface.java
@@ -1,24 +1,38 @@
 package Interfaces;
+class Employee{
+    int id;
+    String name;
 
-class Interface{
-    Interface(){
-        System.out.println("I am an constructor of Interface class");
-
+    // Constructor for initial value
+    public Employee(int i, String n){
+        id = i;
+        name = n;
     }
-    void calle(String c){
-        c = "Hello I am an function of interface";
-        System.out.println(c);
+     // function for giving value to employee
+    void setId(int i){
+        id = i;
+    }
+    void setName(String n){
+        name = n;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
-public class HowToInterface {
-    public static void main(String[]args) {
 
-        Interface In = new Interface();
+public class HowToInterface{
+    public static void main() {
 
-
-
-
+        Employee e = new Employee(0,null);
+        e.setId(55);
+        e.setName("krish");
+        e.getName();
+        e.getId()
 
 
     }
